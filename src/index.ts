46ccfc2c865aff97fdbccc0d30f4238f4c39f674
Utkarsh Shigihalli <onlyutkarsh@users.noreply.github.com>
2021-12-30 22:22:21 +0000

    prettier + middleware

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,46 @@
-import * as dotenv from 'dotenv'
-import cors from 'cors'
-import helmet from 'helmet'
-import express, { Request, Response } from 'express'
-import { BaseItem } from './models/BaseItem'
-import { timeStamp } from 'console'
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import helmet from 'helmet';
+import express, { Request, Response } from 'express';
+import { BaseItem } from './models/BaseItem';
+import { timeStamp } from 'console';
+import { errorHandler } from './middleware/error-middleware';
+import { notFoundHandler } from './middleware/notfound-middleware';
 
-dotenv.config()
+dotenv.config();
 
 if (!process.env.PORT) {
-    process.exit(1)
+    process.exit(1);
 }
 
-const PORT: number = parseInt(process.env.PORT as string, 10)
+const PORT: number = parseInt(process.env.PORT as string, 10);
 
-const app = express()
+const app = express();
 
-app.use(helmet())
-app.use(cors())
-app.use(express.json())
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
 
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`)
-})
+    console.log(`Listening on port ${PORT}`);
+});
 
 app.post('/api/dummy', (req: Request, res: Response) => {
     try {
-        var body: BaseItem = req.body
+        var body: BaseItem = req.body;
         if (!body.id) {
-            res.status(400).send('Missing id')
-            return
+            res.status(400).send('Missing id');
+            return;
         }
         var response = {
             message: `Recieved ID ${body.id}`,
             timeStamp: new Date().toISOString(),
-        }
-        res.status(200).send(response)
+        };
+        res.status(200).send(response);
     } catch (e: any) {
-        res.status(500).send(e.message)
+        res.status(500).send(e.message);
     }
-})
+});
+
+app.use(errorHandler);
+app.use(notFoundHandler);
