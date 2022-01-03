46904d1e7458a8614ca6509901e12f852534fcd7
Utkarsh Shigihalli <onlyutkarsh@users.noreply.github.com>
2022-01-03 17:19:04 +0000

    k8s deployment + service + remove prettier to use editorconfig

--- readme.md
+++ readme.md
@@ -1,4 +1,86 @@
 
+# Simple Node API exposed via K8s Service 
+
+- [Simple Node API exposed via K8s Service](#simple-node-api-exposed-via-k8s-service)
+  - [Typescript](#typescript)
+    - [Install dependencies](#install-dependencies)
+    - [Run the service](#run-the-service)
+    - [Compile the typescript](#compile-the-typescript)
+  - [Docker](#docker)
+    - [Build image](#build-image)
+    - [Run image locally](#run-image-locally)
+    - [Push the image](#push-the-image)
+  - [Kubernetes](#kubernetes)
+    - [Deploy Deployment and Service](#deploy-deployment-and-service)
+    - [Expose Service](#expose-service)
+    - [Access the service](#access-the-service)
+    - [View logs of all pods and follow](#view-logs-of-all-pods-and-follow)
+  - [References](#references)
+
+## Typescript
+
+### Install dependencies
+```bash
+npm run init
+```
+
+### Run the service
+```bash
+npm run dev
+```
+### Compile the typescript
+```bash
+npm run compile
+```
+
+## Docker
+### Build image
+
+```bash
+npm run docker:build
+```
+### Run image locally
+```bash
+npm run docker:run
+```
+
+### Push the image
+Ensure you are logged in using docker CLI with command `docker login` and password (or PAT token if you have 2FA).
+```bash
+docker push onlyutkarsh/k8s-nodejs-api:latest
+```
+
+## Kubernetes
+
+### Deploy Deployment and Service
+
+```bash
+$ cd k8s/
+$ kubectl apply -f deployment.yml
+$ kubectl apply -f service.yml
+```
+### Expose Service
+
+```bash
+minikube service mynodeapi-svc --url
+```
+You will see the URL something like `http://192.168.64.3:32176`
+
+### Access the service
+
+```bash
+curl -X POST \
+ -H "Content-Type: application/json" \
+ -d '{ "id" : "5" }' \
+http://192.168.64.3:32176/api/dummy
+```
+
+### View logs of all pods and follow
+
+```bash
+k logs -f -l app=pod-k8s-nodejs-api
+```
+
 ## References
 
 - https://nodejs.org/en/docs/guides/nodejs-docker-webapp/
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
-import * as dotenv from 'dotenv';
-import cors from 'cors';
-import helmet from 'helmet';
-import express, { Request, Response } from 'express';
-import { BaseItem } from './models/BaseItem';
-import { errorHandler } from './middleware/error-middleware';
-import { notFoundHandler } from './middleware/notfound-middleware';
-import morgan from 'morgan';
+import * as dotenv from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
+import express, { Request, Response } from "express";
+import { BaseItem } from "./models/BaseItem";
+import { errorHandler } from "./middleware/error-middleware";
+import { notFoundHandler } from "./middleware/notfound-middleware";
+import morgan from "morgan";
 
 dotenv.config();
 
 if (!process.env.PORT) {
-    console.log('PORT not defined');
-    process.exit(1);
+  console.log("PORT not defined");
+  process.exit(1);
 }
 
 console.log(`PORT: ${process.env.PORT}`);
@@ -19,31 +19,31 @@ const PORT: number = parseInt(process.env.PORT as string, 10);
 
 const app = express();
 
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
+  console.log(`Listening on port ${PORT}`);
 });
 
-app.post('/api/dummy', (req: Request, res: Response) => {
-    console.log(`${req.url}`);
-    try {
-        var body: BaseItem = req.body;
-        if (!body.id) {
-            res.status(400).send('Missing id');
-            return;
-        }
-        var response = {
-            message: `Recieved ID ${body.id}`,
-            timeStamp: new Date().toISOString(),
-        };
-        res.status(200).send(response);
-    } catch (e: any) {
-        res.status(500).send(e.message);
+app.post("/api/dummy", (req: Request, res: Response) => {
+  console.log(`${req.url}`);
+  try {
+    var body: BaseItem = req.body;
+    if (!body.id) {
+      res.status(400).send("Missing id");
+      return;
     }
+    var response = {
+      message: `Recieved ID ${body.id}`,
+      timeStamp: new Date().toISOString(),
+    };
+    res.status(200).send(response);
+  } catch (e: any) {
+    res.status(500).send(e.message);
+  }
 });
 
 app.use(errorHandler);
